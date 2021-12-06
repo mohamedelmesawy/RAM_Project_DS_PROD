ad5226d2c41d3060c30502fae3de5e4805ef00ff
mohamedelmesawy1993 <mohamedelmesawy1993@gmail.com>
2021-12-06 22:25:15 +0200

    update ml file

--- ML_Pipeline.py
+++ ML_Pipeline.py
@@ -5,8 +5,9 @@ from git.exc import RepositoryDirtyError
 import pandas as pd
 import mlflow
 
-
-repo = git.Repo('.git')
+REPO_PATH = '.git'
+REQUIRED_TAG = 'v1'
+repo = git.Repo(REPO_PATH)
 
 
 def print_repository_info(repo):
@@ -27,9 +28,6 @@ def print_commit_data(commit, req_ver):
                                      commit.author.name, commit.author.email))
     print('commit.authored_datetime:', str(commit.authored_datetime))
     print(str("count: {} and size: {}".format(commit.count(), commit.size)))
-    pprint.pprint(dir(commit))
-    print('SAMAHA commit.summary', commit.summary)
-    print('SAMAHA commit.name_rev', commit.name_rev)
 
     if req_ver in commit.summary:
         return (str(commit.hexsha))
@@ -37,45 +35,41 @@ def print_commit_data(commit, req_ver):
         return None
 
 
+def get_commit_hexsha_by_tag(repo, required_tag):
+    for tag in repo.tags:
+        if tag.name == required_tag:
+            return str(tag.commit.hexsha)
+
+
 # print_repository_info(repo)
-x = print_commit_data(list(repo.iter_commits('main'))[0], 'v1')
-print(x)
+# x = print_commit_data(list(repo.iter_commits('main'))[0], 'v1')
+# print(x)
 
 
 # check that the repository loaded correctly
-# if not repo.bare:
-#     print('Repo is successfully loaded.')
-#     print_repository_info(repo)
-#     COMMITS_TO_PRINT = 10
+if not repo.bare:
+    print('Repo is successfully loaded.')
+    print_repository_info(repo)
+    COMMITS_TO_PRINT = 10
 
-#     # create list of commits then print some of them to stdout
-#     commits = list(repo.iter_commits('main'))[:COMMITS_TO_PRINT]
-#     print("==================== Commits ====================")
-#     print("length of commits: ", len(commits), "===============")
-#     print(commits)
+    # create list of commits then print some of them to stdout
+    commits = list(repo.iter_commits('main'))[:COMMITS_TO_PRINT]
+    print("==================== Commits ====================")
+    print("length of commits: ", len(commits), "===============")
+    print(commits)
 
-#     number = 0
-#     rev1 = ""
-#     req_ver = "v1"
+    commit_hexsha = get_commit_hexsha_by_tag(repo, REQUIRED_TAG)
+    print(commit_hexsha)
+    resource_url = dvc.api.get_url(
+        path='data/Mall_Customers.csv',
+        repo=REPO_PATH,
+        # rev=commit_hexsha,
+        remote='dvc-RAM-remote'
+    )
+    print(f"RAM==================== {resource_url} ====================")
 
-#     while ((number < len(commits)) and rev1 == ""):
-#         print(f"Number is {number}!")
-#         rev1 = print_commit_data(commits[number], req_ver)
-#         print('HIiiiiiiiiiiiiiii', rev1)
-#         number = number + 1
-
-#     print("rev1", rev1)
-#     print('CHECK MEEEEEEEEE', rev1)
-
-#     resource_url = dvc.api.get_url(
-#         path='data/Mall_Customers.csv',
-#         repo=r".git",
-#         rev=rev1
-#     )
-#     print(f"==================== {resource_url} ====================")
-
-#     df = pd.read_csv(resource_url, sep=',')
-#     print(len(df))
+    # df = pd.read_csv(resource_url, sep=',')
+    # print(len(df))
 #     display(df)
 
 #     mlflow.set_tracking_uri('http://127.0.0.1:5000')
